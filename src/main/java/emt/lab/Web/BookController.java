ecf42fe142639afa28a6207ed1cf7bbd8052d610
LinaNakova <lina.nakova@gmail.com>
2023-03-19 20:13:00 +0100

    - all CRUD actions implemented with response entity - api call for get categories added - pageable method for all books added

--- src/main/java/emt/lab/Web/BookController.java
+++ src/main/java/emt/lab/Web/BookController.java
@@ -1,14 +1,18 @@
 package emt.lab.Web;
 
+import emt.lab.Enum.Category;
 import emt.lab.Model.Book;
-import emt.lab.Model.Request.BookRequest;
+import emt.lab.Model.Request.BookDto;
 import emt.lab.Service.BookService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/api/book")
 public class BookController {
     private final BookService bookService;
 
@@ -21,23 +25,51 @@ public class BookController {
         return this.bookService.findAll();
     }
 
+    @GetMapping("/pageable")
+    public Page<Book> getAllBooksPageable(Pageable pageable) {
+        return this.bookService.findAllPageable(pageable);
+    }
+
     @GetMapping("/{id}")
-    public Book getBookById(@PathVariable Long id) {
-        return this.bookService.findById(id);
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        return this.bookService.findById(id)
+                .map(book -> ResponseEntity.ok().body(book))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
     }
 
-    @PostMapping("/add-book")
-    public void addBook(@RequestBody BookRequest bookRequest) {
-        this.bookService.addBook(bookRequest);
+    @PostMapping("/add")
+    public ResponseEntity<Book> addBook(@RequestBody BookDto bookRequest) {
+        return this.bookService.addBook(bookRequest)
+                .map(book -> ResponseEntity.ok().body(book))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
     }
 
-    @PutMapping("/edit-book/{id}")
-    public void addBook(@RequestBody BookRequest bookRequest, @PathVariable Long id) {
-        this.bookService.editBook(id, bookRequest);
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<Book> addBook(@RequestBody BookDto bookRequest, @PathVariable Long id) {
+        return this.bookService.editBook(id, bookRequest)
+                .map(book -> ResponseEntity.ok().body(book))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
     }
 
-    @DeleteMapping("/delete-book/{id}")
-    public void deleteBook(@PathVariable Long id) {
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteBook(@PathVariable Long id) {
         this.bookService.deleteBook(id);
+        if (this.bookService.findById(id).isEmpty()) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PatchMapping("/mark-as-taken/{id}")
+    public ResponseEntity markBookAsTaken(@PathVariable Long id) {
+        if (this.bookService.markBookAsTaken(id).isEmpty())
+            return ResponseEntity.badRequest().build();
+        else return ResponseEntity.ok().build();
+
+    }
+
+    @GetMapping("/categories")
+    public List<Category> findAllCategories() {
+        return this.bookService.findAllBookCategories();
     }
 }
--- src/main/java/emt/lab/Web/CountryController.java
+++ src/main/java/emt/lab/Web/CountryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/countries")
+@RequestMapping("/api/countrie")
 public class CountryController {
 
     private final CountryService countryService;
