3401df3600e08f2ad62066914142241ffca2ce8c
LinaNakova <lina.nakova@gmail.com>
2023-03-19 20:13:25 +0100

    .

--- src/main/java/emt/lab/Web/CountryController.java
+++ src/main/java/emt/lab/Web/CountryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/countrie")
+@RequestMapping("/api/country")
 public class CountryController {
 
     private final CountryService countryService;
